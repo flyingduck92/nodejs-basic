37e29598298654d6d704943cab3b7fa470a81a99
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-01 05:09:24 +0700

    parsing request body

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  const url = req.url
+  const [url, method] = [req.url, req.method]
 
   if (url === '/') {
     res.write('<html>')
@@ -15,6 +16,23 @@ const server = http.createServer((req, res) => {
       </body>
     `)
     res.write('</html>')
+    return res.end()
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = []
+    req.on('data', (chunk) => {
+      body.push(chunk)
+      console.log(chunk)
+    })
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString()
+      const message = parsedBody.split('=')[1]
+      fs.writeFileSync('message.txt', message)
+    })
+
+    res.statusCode = 302
+    res.setHeader('Location', '/')
     return res.end()
   }
 
