f8b78f641f1b737de0aaad229aa7e6a0c087b116
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-01 04:32:51 +0700

    send response methods to client

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers)
   // process.exit()
+
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<head><title>Hello World</title></head>')
+  res.write('<body><h1>Hello from nodejs</h1></body>')
+  res.write('</html>')
+  res.end()
 })
 
 server.listen(3000)
