f219c30c789607d681492d2ae8c21e68d143ef35
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-01 11:18:48 +0700

    Add express as backend server

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const http = require('http')
-const routes = require('./routes')
+const express = require('express')
 
-const server = http.createServer(routes.handler)
+const app = express()
 
-server.listen(3000)
+app.use((req, res, next) => {
+  console.log('In the middleware')
+  next() // allowed request to next one middleware
+})
+
+app.use((req, res, next) => {
+  console.log('In another middleware')
+  res.send('<h1>Hello from express</h1>')
+})
+
+app.listen(3000)
