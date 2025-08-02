1d564d38eaf5b3449b8efc54241dcf481cdbe187
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-02 07:44:39 +0700

    serving html pages and 404 page

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
+const path = require('path')
 const express = require('express')
 
 const router = express.Router()
 
 router.get('/add-product', (req, res, next) => {
   console.log('In /add-product middleware')
-  res.send('<form action="/admin/products" method="POST"><input name="item" type="text" /><button type="submit">Submit</button></form>')
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 router.post('/products', (req, res, next) => {
