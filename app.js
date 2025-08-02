1c03d7b7a8bed6c87bbac618d128c944752a12b0
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-02 09:40:20 +0700

    removing body-parser (deprecated)

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const path = require('path')
 const express = require('express')
-const bodyParser = require('body-parser')
 
 const app = express()
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes)
