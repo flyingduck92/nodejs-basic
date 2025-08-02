c5d9433ff6e51c0f8da3398cbe89410ffd6d51eb
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-02 08:52:17 +0700

    add navigation style and main element on html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
 const path = require('path')
 const express = require('express')
 
+const rootDir = require('../utils/path')
+
 const router = express.Router()
 
 router.get('/add-product', (req, res, next) => {
   console.log('In /add-product middleware')
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
-router.post('/products', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body)
-  res.redirect('/add-product')
+  res.redirect('/')
 })
 
 module.exports = router
