cec5c1f0101ecdc1a3e0ef9523356badfad34c81
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-03 07:50:16 +0700

    Using ejs templating engine

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const expressHandleBars = require('express-handlebars')
 
 const app = express()
 
+/* ejs Templating Engine */
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
 /* Handlebars Templating Engine */
-app.engine('.hbs', expressHandleBars.engine({ extname: '.hbs', layoutsDir: 'views/layouts', defaultLayout: 'main-layout' }))
-app.set('view engine', '.hbs')
-app.set('views', './views')
+// app.engine('.hbs', expressHandleBars.engine({ extname: '.hbs', layoutsDir: 'views/layouts', defaultLayout: 'main-layout' }))
+// app.set('view engine', '.hbs')
+// app.set('views', './views')
 
 /* Pug Templating Engine */
 // app.set('view engine', 'pug')
@@ -24,7 +28,7 @@ app.use(shopRoutes)
 
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-  res.status(404).render('404', { layout: false, pageTitle: 'Page Not Found' })
+  res.status(404).render('404', { layout: false, pageTitle: 'Page Not Found', path: '' })
 })
 
 app.listen(3000)
