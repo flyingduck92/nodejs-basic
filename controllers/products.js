1deb322cdc3ecc307003b90b018adb13b6468c99
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-08-06 09:05:35 +0700

    split navigation from layout and add new (blank) html pages

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 module.exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product'
   })
@@ -17,7 +17,7 @@ module.exports.postAddProduct = (req, res, next) => {
 
 module.exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render('shop/product-list', {
       products,
       pageTitle: 'My Shop',
       path: '/'
